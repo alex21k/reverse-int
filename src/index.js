1540848511175a49d086f52b318b01f0cb72e396
alex21k <77678805+alex21k@users.noreply.github.com>
2021-03-15 19:05:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 module.exports = function reverse (n) {
     let str = n.toString();
-    
     str = str.split('').reverse().join('');
-    
     let x = Number.parseInt(str);
     console.log(x);
     return x;
